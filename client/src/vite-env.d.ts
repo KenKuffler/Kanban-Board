afbc90de52ef10b67511752faf3431d724188f0d
KenKuffler <kennethrkuffler@hotmail.com>
2024-11-11 16:48:51 -0600

    client deploy 4

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL_LOCAL: string;
+    readonly VITE_API_URL_PRODUCTION: string;
+    // Add other environment variables as needed
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -8,12 +9,12 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_URL,
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
       },
       '/auth': {
-        target: import.meta.env.VITE_API_URL,
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
       },
