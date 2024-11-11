06f6754514e642c21d16e411808450ac3d6c566b
KenKuffler <kennethrkuffler@hotmail.com>
2024-11-11 17:59:05 -0600

    client deploy 11

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,22 +1,20 @@
 import axios from 'axios';
 import { UserLogin } from "../interfaces/UserLogin";
 
-// Set API URL based on environment
-const API_URL = import.meta.env.PROD
-  ? import.meta.env.VITE_API_URL_PRODUCTION
-  : import.meta.env.VITE_API_URL_LOCAL;
+// Environment-specific base URL
+const API_URL = `${import.meta.env.VITE_API_URL}/auth/login`; 
 
-// Function to log in the user
 const login = async (userInfo: UserLogin) => {
   try {
-    const response = await axios.post(API_URL, userInfo); // Send POST request to login route
-    return response.data; // Return the response (which should include the JWT token)
+    const response = await axios.post(API_URL, userInfo); 
+    return response.data; 
   } catch (error) {
-    throw new Error('Failed to log in. Please check your credentials.'); // Handle login errors
+    throw new Error('Failed to log in. Please check your credentials.');
   }
 };
 
 export { login };
+
 
 
 
