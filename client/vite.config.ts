7fd81ba49e705745f581f73e54558ffb765b7f7c
KenKuffler <kennethrkuffler@hotmail.com>
2024-11-11 16:43:46 -0600

    client deploy 3

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,27 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     port: 3000,
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: import.meta.env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
       },
       '/auth': {
-        target: 'http://localhost:3001',
+        target: import.meta.env.VITE_API_URL,
         changeOrigin: true,
-        secure: false
+        secure: false,
       },
     },
   },
+  build: {
+    outDir: 'dist',
+  },
 });
+
+
