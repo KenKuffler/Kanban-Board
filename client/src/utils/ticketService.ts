8f9de6acf5cd6b3d0e7e17a237bdd073c51d1099
KenKuffler <kennethrkuffler@hotmail.com>
2024-11-14 18:22:37 -0600

    unified deploy fixes 16

---
+++ client/src/utils/ticketService.ts
@@ -0,0 +1,50 @@
+// src/utils/ticketService.ts
+import { TicketData } from '../interfaces/TicketData';
+import { ApiMessage } from '../interfaces/ApiMessage';
+import Auth from './auth';
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+// Function to retrieve all tickets
+export const retrieveTickets = async (): Promise<TicketData[]> => {
+  try {
+    const response = await fetch(`${API_URL}/api/tickets/`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${Auth.getToken()}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Invalid API response, check network tab!');
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error retrieving tickets:', error);
+    return [];
+  }
+};
+
+// Function to delete a ticket by ID
+export const deleteTicket = async (ticketId: number): Promise<ApiMessage> => {
+  try {
+    const response = await fetch(`${API_URL}/api/tickets/${ticketId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${Auth.getToken()}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to delete ticket');
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error deleting ticket:', error);
+    return { message: 'Failed to delete ticket' };
+  }
+};
+
